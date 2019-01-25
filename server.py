3aae87d8b7b3a5de7f648dc48f25f5214cc2eeb4
Matthew Kluk <mkluk@ualberta.ca>
2019-01-25 01:18:55 -0500

    Got it to display the pages, but failed the tests

--- server.py
+++ server.py
@@ -30,107 +30,168 @@ import os # to handle finding files and directories
 
 class MyWebServer(socketserver.BaseRequestHandler):
 
-    """
-        This checks if the file can be reached.
-        If it can, serve the file. If not, send 404.
-    """
+    # This checks if the file can be reached.
+    # If it can, serve the file. If not, send 404.
     def checkIfCanGet(self, request_array):
-        '''
-            Need to get the path that the files to
-            display are in. 
+        # Need to get the path that the files to
+        # display are in. 
 
-            We are not allowed to hardcode, so we need
-            to get the current directory that we're in.
+        # We are not allowed to hardcode, so we need
+        # to get the current directory that we're in.
 
-            https://www.techcoil.com/blog/how-to-get-the-directory-path-of-a-python-3-script-from-within-itself/
-        '''
+        # https://www.techcoil.com/blog/how-to-get-the-directory-path-of-a-python-3-script-from-within-itself/
         serverDirectory = os.path.dirname(os.path.realpath(__file__))
 
         # Get path to file, checks if it exists, serve if yes, 404 if no
         filepath = serverDirectory + "/www" + request_array[1]
-        print(filepath)
-        if os.path.exists('/home/student/Assignments/Assignment1/CMPUT404-assignment-webserver/www/deep'):
-            print("Without slash success")
-        if os.path.exists('/home/student/Assignments/Assignment1/CMPUT404-assignment-webserver/www/deep/'):
-            print("With slash success")
 
-        '''
-            Need to check if file exists.
-            If it doesn't exist, give a 404 response.
-            If it does exist, check if it's a folder.
-            If it is a folder, check if it has a slash at the end
-            If there isn't one, add it, so we can easily access the
-            HTML file. 
+        # Code to test finding the filepaths
 
-            https://www.techcoil.com/blog/how-to-get-the-directory-path-of-a-python-3-script-from-within-itself/
-            https://dbader.org/blog/python-check-if-file-exists
-            https://stackoverflow.com/questions/3204782/how-to-check-if-a-file-is-a-directory-or-regular-file-in-python
-        '''
-    
-    def serveFile(self, request_array):
-        pass
+        # print(filepath)
+        # if os.path.exists('/home/student/Assignments/Assignment1/CMPUT404-assignment-webserver/www/deep'):
+            # print("Without slash success")
+        # if os.path.exists('/home/student/Assignments/Assignment1/CMPUT404-assignment-webserver/www/deep/'):
+        #   # print("With slash success")
 
+        # Need to check if file exists.
+        # If it doesn't exist, give a 404 response.
+        # If it does exist, check if it's a folder.
+        # If it is a folder, check if it has a slash at the end
+        # If there isn't one, add it, so we can easily access the
+        # HTML file. 
+
+        # https://www.techcoil.com/blog/how-to-get-the-directory-path-of-a-python-3-script-from-within-itself/
+        # https://dbader.org/blog/python-check-if-file-exists
+        # https://stackoverflow.com/questions/3204782/how-to-check-if-a-file-is-a-directory-or-regular-file-in-python
+        if os.path.exists(filepath):
+            if os.path.isdir(filepath):
+                if not filepath.endswith('/'):
+                    filepath += "/"
+                filepath += "index.html" # making sure we go to the index.html
+                if not os.path.exists(filepath):
+                    self.serve404()
+                else:
+                    self.serveFile(filepath)
+            else:
+                self.serveFile(filepath)
+        else:
+            self.serve404()
+
+    # Since the filepath has already been verified to exist,
+    # just send over the contents in a 200 OK response. 
+
+    # I learned about formatting HTTP responses here:
+    # https://www.tutorialspoint.com/http/http_responses.htm
+
+    # and about mime-codes here:
+    # https://www.sitepoint.com/mime-types-complete-list/
+    def serveFile(self, filepath):
+        # Get the mime-type
+        if filepath.endswith("html"):
+            mime_type = "text/html"
+        elif filepath.endswith("css"):
+            mime_type = "text/css"
+
+        # Get the status code
+        code = "200 OK"
+
+        # Read in the HTML/CSS
+        with open(filepath, 'r') as read_file:
+            text = read_file.read()
+
+        self.sendResponse(code, mime_type, text)
+
+    # Get the 404 code and HTML message
     def serve404(self):
-        pass
+        code = "404 Not Found"
+        mime_type = "text/html"
+
+        # Note: the guide on making HTML messages
+        # also comes from:
+        # https://www.tutorialspoint.com/http/http_responses.htm
+        text = "<html><head>\r\n<title>404 Not Found</title>" + \
+        "\r\n</head><body>\r\n<h1>Not Found</h1>\r\n<p>The " + \
+        "requested URL was not found</p>\r\n</body></html>"
+
+        self.sendResponse(code, mime_type, text) 
     
+    # Get the 405 code and HTML message.
     def serve405(self):
-        pass
+        code = "405 Method Not Allowed"
+        mime_type = "text/html"
+
+        text = "<html><head>\r\n<title>405 Method Not Allowed</title>" + \
+        "\r\n</head><body>\r\n<h1>Method Not Allowed</h1>\r\n<p>This " + \
+        "method is not allowed in a request</p>\r\n</body></html>"
+
+        self.sendResponse(code, mime_type, text) 
+
+    # Finally sending the response over.
+    # Python formatting found at:
+    # https://realpython.com/python-string-formatting/
+    def sendResponse(self, code, mime_type, text):
+        httpResponse = "HTTP/1.1 {code}\r\n" + \
+        "Content-Length: {length}\r\n" + \
+        "Content-Type: {mime}\r\n" + \
+        "Connection: Closed\r\n\r\n" + \
+        "{text}\r\n".format(code=code, length = str(len(text)), mime=mime_type, text=text)
+
+        # finally send the response to the client
+        self.request.sendall(httpResponse.encode())
 
     
     # According to https://docs.python.org/2/library/socketserver.html,
-    # This is to do all of the work required to service a request
+    # This is to do all of the work required to service a request.
     def handle(self):
         self.data = self.request.recv(1024).strip()
         # print ("Got a request of: %s\n" % self.data)
 
-        '''
-            This code sees how the request data is represented
-            in Python
-        '''
-        print(self.data)
-        print(type(self.data))
-        '''
-            This code was used to identify the different parts
-            of the HTML request
-        '''
-        element_table = self.data.split()
-        for element in element_table:
-            print(element, element_table.index(element))
 
-        '''
-            This code was me looking at converting byte
-            literals to strings, inspired by this link
-            https://stackoverflow.com/questions/6269765/what-does-the-b-character-do-in-front-of-a-string-literal
-        '''
-        print(type(element_table[0]))
-        print(type(element_table[0].decode('utf-8')))
-        print(element_table[0].decode('utf-8'))
-        for element in element_table:
-            element.decode('utf-8')
-        print(element_table)
+        # This code sees how the request data is represented
+        # in Python
+        # print(self.data)
+        # print(type(self.data))
 
-        '''
-            Take request, split it into an array, and decode each element into strings.
-            Check if the method of the request has "GET" or not,
-            then we can serve the correct response.
-            Forgot about list comprehensions.
-            https://stackoverflow.com/questions/3371269/call-int-function-on-every-list-element
-        '''
+
+        # This code was used to identify the different parts
+        # of the HTML request
+        # element_table = self.data.split()
+        # for element in element_table:
+            # print(element, element_table.index(element))
+
+
+        # This code was me looking at converting byte
+        # literals to strings, inspired by this link
+        # https://stackoverflow.com/questions/6269765/what-does-the-b-character-do-in-front-of-a-string-literal
+        # print(type(element_table[0]))
+        # print(type(element_table[0].decode('utf-8')))
+        # print(element_table[0].decode('utf-8'))
+        # for element in element_table:
+            # element.decode('utf-8')
+        # print(element_table)
+
+
+        # Take request, split it into an array, and decode each element into strings.
+        # Check if the method of the request has "GET" or not,
+        # then we can serve the correct response.
+        # Forgot about list comprehensions.
+        # https://stackoverflow.com/questions/3371269/call-int-function-on-every-list-element
+
         request_array = [ element.decode('utf-8') for element in self.data.split() ]
-        print(request_array)
+        # print(request_array)
         method = request_array[0]
-        print(len(method))
-        print(type(method))
+        # print(len(method))
+        # print(type(method))
         if method != "GET":
             self.serve405()
-            print("405")
+            # print("405")
         else:
             self.checkIfCanGet(request_array)
-            print("Success!")
+            # print("Success!")
 
 
         # This code sends back just the word OK.
-        self.request.sendall(bytearray("OK",'utf-8'))
+        # self.request.sendall(bytearray("OK",'utf-8'))
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080 # will run on http://127.0.0.1:8080
